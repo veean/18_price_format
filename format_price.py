7d598c4adcc62bb71502a675721e325c54bb97dc
veean <nersesowm@gmail.com>
2017-01-23 02:36:12 +0400

    add skeleton core

--- format_price.py
+++ format_price.py
@@ -1,5 +1,35 @@
+import argparse
+
+
+def choose_formatting(price):
+    if price.is_integer():
+        return ',.0f'
+    else:
+        return ',.2f'
+
+
 def format_price(price):
-    pass
+    if price > 0:
+        try:
+            formatted_price = float(price)
+        except (ValueError, TypeError):
+            return None
+    else:
+        return None
+
+
+
+
 
 if __name__ == '__main__':
-    pass
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('price', help='specify price to make it beautiful')
+    # users_price = parser.parse_args().price
+
+    users_price = input('price : ')
+
+    formatted_price = users_price.replace(',', '.')
+
+    pp = float(formatted_price)
+
+    print(format(pp, ',.2f').replace(',', ' '))
--- tests.py
+++ tests.py
@@ -1,1 +1,26 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_solves_real_roots(self):
+        beautiful_price = format_price(1)
+        self.assertEqual(beautiful_price, 1)
+
+    def test_negative_price(self):
+        beautiful_price = format_price(-56.01)
+        self.assertEqual(beautiful_price, None)
+
+    def test_second_root_is_none_if_one_solution(self):
+        beautiful_price = format_price(1)
+        self.assertIsNotNone(beautiful_price, 1)
+
+    def test_returns_none_for_complex_solution(self):
+        beautiful_price = format_price(1)
+        self.assertIsNone(beautiful_price, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
